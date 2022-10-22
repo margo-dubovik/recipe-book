748631c906bae3dfff4b001fba42130700a201b8
margo-dubovik <margodubovik531@gmail.com>
2022-10-22 22:35:58 +0300

    making webhook

--- recipebook/tgbot/urls.py
+++ recipebook/tgbot/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('telegram-webhook', views.webhook, name='telegram-webhook'),
+        path('telegram-webhook/', views.webhook, name='telegram-webhook'),
     ]
