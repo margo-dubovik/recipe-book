17a3f2dc7cc1bb3dbe16892d37821347be0ec653
margo-dubovik <margodubovik531@gmail.com>
2022-10-22 22:46:59 +0300

    redirect to admin-panel done for convenience

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('home-view')), name='redirect-to-admin-panel'),
     path('admin-panel/', include('admin_panel.urls')),
     path('tgbot/', include('tgbot.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
