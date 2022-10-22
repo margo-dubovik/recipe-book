1c586c99451c28c9a75378e5e78d7d36166f4344
margo-dubovik <margodubovik531@gmail.com>
2022-10-22 22:07:01 +0300

    making webhook

--- recipebook/tgbot/management/commands/bot.py
+++ recipebook/tgbot/management/commands/bot.py
@@ -186,4 +186,5 @@ class Command(BaseCommand):
     help = 'Recipe Bot'
 
     def handle(self, *args, **kwargs):
-        bot.infinity_polling()
+        # bot.infinity_polling()
+        bot.set_webhook(url="https://recipebook-margodubovik.herokuapp.com")
