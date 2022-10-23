2f7c5ea22e60c5b8fc57d98839ba939fa4a32654
margo-dubovik <margodubovik531@gmail.com>
2022-10-23 13:23:10 +0300

    all finished

--- recipebook/tgbot/views.py
+++ recipebook/tgbot/views.py
@@ -7,7 +7,7 @@ from tgbot.management.commands.bot import bot
 
 @csrf_exempt
 def webhook(request):
-    print(request.body.decode('utf-8'))
+    # print(request.body.decode('utf-8'))
     update = types.Update.de_json(json.loads(request.body.decode('utf-8')))
     bot.process_new_updates([update])
     return JsonResponse({'message': 'OK'}, status=200)
